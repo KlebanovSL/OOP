ce6d8ff6cf275fed388d226210a6928f803e9ab0
KlebanovSL <Ki2Style@yandex.by>
2022-08-12 09:30:24 +0300

    'changed'

--- task_4.py
+++ task_4.py
@@ -1,26 +1,28 @@
 class Kg:
-    def __init__(self, value: float, measure_of_weight: str):
-        self.value = value
-        self.measure_of_weight = measure_of_weight
-    def make_kg(self):
-        '''Печатает значение в килограммах 1 тонна = 1000 кг, 1 центнер = 100 кг, 1 фунт = 0,45359237 кг, 1 пуд = 16,38 кг'''
-        if 'тонн' in self.measure_of_weight:
-            print(f"{self.value*1000} 'килограмм'")
-        elif 'центнер' in self.measure_of_weight:
-            print(f"{self.value * 100} 'килограмм'")
-        elif 'фунт' in self.measure_of_weight:
-            print(f"{self.value * 0.45359237} 'килограмм'")
-        elif 'пуд' in self.measure_of_weight:
-            print(f"{self.value * 16.38} 'килограмм'")
-        else:
-            print('Неверно указан формат веса!')
-a = Kg(2, "тонны")
-b = Kg(36, "центнера")
-c = Kg(16, "фунтов")
-d = Kg(2, "пуда")
-e = Kg(4, "милиграмма")
-a.make_kg()
-b.make_kg()
-c.make_kg()
-d.make_kg()
-e.make_kg()
+    """ Содержит методы перевода из заданных мер веса в килограммы"""
+
+    @staticmethod
+    def get_from_tonn(value: float):
+        """ Печатает значение в килограммах 1 тонна = 1000 кг """
+        print(f"{value*1000:.2f} килограмм")
+
+    @staticmethod
+    def get_from_centner(value: float):
+        """ Печатает значение в килограммах 1 центнер = 100 кг """
+        print(f"{value * 100:.2f} килограмм")
+
+    @staticmethod
+    def get_from_funt(value: float):
+        """ Печатает значение в килограммах 1 фунт = 0,45359237 кг"""
+        print(f"{value * 0.45359237:.2f} килограмм")
+
+    @staticmethod
+    def get_from_pud(value: float):
+        """ Печатает значение в килограммах 1 пуд = 16,38 кг"""
+        print(f"{value * 16.38:.2f} килограмм")
+
+
+Kg.get_from_tonn(3)
+Kg.get_from_centner(4)
+Kg.get_from_funt(5)
+Kg.get_from_pud(6)
